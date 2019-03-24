10960ad3b424b9e831549bd9733ca1efb29dbab9
Erik Hergins <hergins1@hotmail.com>
2019-03-23 23:34:53 -0500

    Starting JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,19 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+var userGuess = function() {
+
+    var answer = $('choice');
+    var val= "";
+    for (var i = 0, length = answer.length; i < length; i++) {
+        if (answer[i].checked) {
+           val = answer[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please select choice answer');
+    } else if ( val == "Scripting" ) {
+      alert('Answer is correct !');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
